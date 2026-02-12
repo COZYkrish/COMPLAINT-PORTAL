61667253298a464b25d7103fe32648c4322c4589
COZYkrish <cozykrish2916@gmail.com>
2026-02-12 12:21:46 +0530

    Added Spring Boot main application file

---
+++ src/main/java/com/krish/complaintportal/ComplaintportalApplication.java
@@ -0,0 +1,12 @@
+package com.krish.complaintportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintportalApplication.class, args);
+    }
+}
