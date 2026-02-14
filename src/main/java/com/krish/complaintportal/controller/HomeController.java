6e023b2b62fa450c0c5422d9bf98f678a8ed328b
COZYkrish <cozykrish2916@gmail.com>
2026-02-14 16:12:10 +0530

    Added registration logic in controller

--- src/main/java/com/krish/complaintportal/controller/HomeController.java
+++ src/main/java/com/krish/complaintportal/controller/HomeController.java
@@ -1,13 +1,38 @@
 package com.krish.complaintportal.controller;
 
+import com.krish.complaintportal.model.User;
+import com.krish.complaintportal.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
+    private final UserService userService;
+
+    // Constructor Injection
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String password) {
+
+        User user = new User(name, email, password);
+        userService.saveUser(user);
+
+        return "redirect:/";
     }
 }
