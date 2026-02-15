f82023b0ffe353f144faebd9ef9b78802b629885
COZYkrish <cozykrish2916@gmail.com>
2026-02-15 12:06:38 +0530

    Day 3 - Commit 13: Implemented BCrypt password matching

--- src/main/java/com/krish/complaintportal/controller/AuthController.java
+++ src/main/java/com/krish/complaintportal/controller/AuthController.java
@@ -1,14 +1,16 @@
 package com.krish.complaintportal.controller;
 
 import com.krish.complaintportal.repository.UserRepository;
+import com.krish.complaintportal.model.User;
 
 import jakarta.servlet.http.HttpSession;
 
-import com.krish.complaintportal.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
 public class AuthController {
@@ -16,34 +18,58 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String email,
+                               @RequestParam String password,
+                               Model model) {
+
+        if (userRepository.findByEmail(email) != null) {
+            model.addAttribute("error", "Email already registered!");
+            return "register";
+        }
+        User user = new User();
+        user.setEmail(email);
+
+        user.setPassword(passwordEncoder.encode(password));
+
+        userRepository.save(user);
+
+        return "redirect:/login";
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
     @PostMapping("/login")
-     public String loginUser(@RequestParam String email,
-        @RequestParam String password,
-        Model model,
-        HttpSession session) {
+    public String loginUser(@RequestParam String email,
+                            @RequestParam String password,
+                            Model model,
+                            HttpSession session) {
 
-    User user = userRepository.findByEmail(email);
+        User user = userRepository.findByEmail(email);
 
-    if (user == null) {
-        model.addAttribute("error", "Email not registered!");
-        return "login";
+        if (user == null) {
+            model.addAttribute("error", "Email not registered!");
+            return "login";
+        }
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            model.addAttribute("error", "Invalid password!");
+            return "login";
+        }
+
+        session.setAttribute("loggedInUser", user);
+
+        return "redirect:/dashboard";
     }
-
-    if (!user.getPassword().equals(password)) {
-        model.addAttribute("error", "Invalid password!");
-        return "login";
-    }
-
-    session.setAttribute("loggedInUser", user);
-
-    return "redirect:/dashboard";
-}
-
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, HttpSession session) {
@@ -53,13 +79,15 @@ public class AuthController {
         if (user == null) {
             return "redirect:/login";
         }
+
         model.addAttribute("user", user);
         return "dashboard";
-}
+    }
+
+
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/login";
     }
-
 }
