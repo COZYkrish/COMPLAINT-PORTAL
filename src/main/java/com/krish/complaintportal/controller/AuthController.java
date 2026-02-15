e144c939c5ad57ed26ed180380976abbff40f09a
COZYkrish <cozykrish2916@gmail.com>
2026-02-15 11:29:54 +0530

    Day 3 - Commit 6: Added session handling after login

--- src/main/java/com/krish/complaintportal/controller/AuthController.java
+++ src/main/java/com/krish/complaintportal/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.krish.complaintportal.controller;
 
 import com.krish.complaintportal.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
 import com.krish.complaintportal.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,28 +22,32 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String loginUser(@RequestParam String email,
-                            @RequestParam String password,
-                            Model model) {
+     public String loginUser(@RequestParam String email,
+        @RequestParam String password,
+        Model model,
+        HttpSession session) {
 
-        User user = userRepository.findByEmail(email);
+    User user = userRepository.findByEmail(email);
 
-        if (user == null) {
-            model.addAttribute("error", "Email not registered!");
-            return "login";
-        }
-
-        if (!user.getPassword().equals(password)) {
-            model.addAttribute("error", "Invalid password!");
-            return "login";
-        }
-
-        return "redirect:/dashboard";
+    if (user == null) {
+        model.addAttribute("error", "Email not registered!");
+        return "login";
     }
+
+    if (!user.getPassword().equals(password)) {
+        model.addAttribute("error", "Invalid password!");
+        return "login";
+    }
+
+    session.setAttribute("loggedInUser", user);
+
+    return "redirect:/dashboard";
+}
+
 
     @GetMapping("/dashboard")
     public String dashboard() {
         return "dashboard";
     }
-    
+
 }
