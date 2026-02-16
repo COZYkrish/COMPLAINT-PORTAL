1f58569c1bc6a5b805a773475764445dc025072e
COZYkrish <cozykrish2916@gmail.com>
2026-02-16 22:19:14 +0530

    Updated findByEmail to return Optional<User>

--- src/main/java/com/krish/complaintportal/repository/UserRepository.java
+++ src/main/java/com/krish/complaintportal/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.krish.complaintportal.repository;
 import com.krish.complaintportal.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    
+Optional<User> findByEmail(String email);
 
 }
