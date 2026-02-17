69c71c3418b87e5bbaf74148f0544b23b1b0e6e9
COZYkrish <cozykrish2916@gmail.com>
2026-02-17 12:16:25 +0530

    created complaint page for user

---
+++ src/main/java/com/krish/complaintportal/controller/DashboardController.java
@@ -0,0 +1,19 @@
+package com.krish.complaintportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+
+        model.addAttribute("email", principal.getName());
+
+        return "dashboard";
+    }
+}
