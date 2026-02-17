731efb04ec158991ba6a7b051bb28eb92b4316d0
COZYkrish <cozykrish2916@gmail.com>
2026-02-17 11:56:59 +0530

    Integrated Spring Security with CustomUserDetailsService for DB authentication

--- src/main/java/com/krish/complaintportal/controller/AuthController.java
+++ src/main/java/com/krish/complaintportal/controller/AuthController.java
@@ -2,9 +2,6 @@ package com.krish.complaintportal.controller;
 
 import com.krish.complaintportal.repository.UserRepository;
 import com.krish.complaintportal.model.User;
-
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,69 +22,28 @@ public class AuthController {
     public String registerPage() {
         return "register";
     }
+
     @PostMapping("/register")
     public String registerUser(@RequestParam String email,
                                @RequestParam String password,
                                Model model) {
 
-        if (userRepository.findByEmail(email) != null) {
+        if (userRepository.findByEmail(email).isPresent()) {
             model.addAttribute("error", "Email already registered!");
             return "register";
         }
         User user = new User();
         user.setEmail(email);
-
         user.setPassword(passwordEncoder.encode(password));
+        user.setRole("ROLE_USER");
 
         userRepository.save(user);
 
         return "redirect:/login";
     }
-
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String loginUser(@RequestParam String email,
-                            @RequestParam String password,
-                            Model model,
-                            HttpSession session) {
-
-        User user = userRepository.findByEmail(email).orElse(null);
-
-        if (user == null) {
-            model.addAttribute("error", "Email not registered!");
-            return "login";
-        }
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            model.addAttribute("error", "Invalid password!");
-            return "login";
-        }
-
-        session.setAttribute("loggedInUser", user);
-
-        return "redirect:/dashboard";
-    }
-
-    @GetMapping("/dashboard")
-    public String dashboard(Model model, HttpSession session) {
-
-        User user = (User) session.getAttribute("loggedInUser");
-
-        if (user == null) {
-            return "redirect:/login";
-        }
-
-        model.addAttribute("user", user);
-        return "dashboard";
-    }
-
-
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:/login";
-    }
 }
